56d740d74b2b25493fa3f536c99f1f20e0a08c64
Полина <polina_kazhina@mail.ru>
2023-06-17 16:37:34 +0300

    feat: make date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const dateCur = new Date(new Date(date).getFullYear(), 1, 29);
+  return dateCur.getMonth() === 1;
 }
 
 
@@ -73,8 +74,13 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const rez = new Date(endDate - startDate);
+  const h = (`00${rez.getUTCHours()}`).slice(-2);
+  const m = (`00${rez.getUTCMinutes()}`).slice(-2);
+  const s = (`00${rez.getUTCSeconds()}`).slice(-2);
+  const ms = (`000${rez.getUTCMilliseconds()}`).slice(-3);
+  return (`${h}:${m}:${s}.${ms}`);
 }
 
 
@@ -94,8 +100,13 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const rez = new Date(date);
+  const h = rez.getUTCHours() >= 12 ? rez.getUTCHours() - 12 : rez.getUTCHours();
+  const m = rez.getUTCMinutes();
+  const rad = Math.PI / 180;
+  const angle = Math.abs((rad / 2) * (60 * h - 11 * m));
+  return Math.min(angle, 2 * Math.PI - angle);
 }
 
 
